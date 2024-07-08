b698e21690ab305c05e147d26844401b9fbcb293
Sebastien Zhou <Sebastien.4084@gmail.com>
2024-07-08 16:40:31 +0200

    finito

--- python/src/consumer.py
+++ python/src/consumer.py
@@ -7,16 +7,22 @@ from environment import KAFKA_TOPIC, KAFKA_BROKER
 def process_data(message):
     data = message.value
 
-    col1, col2 = st.columns(2)
+    if 'initialized' not in st.session_state:
+        st.session_state['col1'], st.session_state['col2'] = st.columns(2)
+        st.session_state['location'] = st.session_state['col1'].empty()
+        st.session_state['region'] = st.session_state['col2'].empty()
+        st.session_state['temperature'] = st.session_state['col2'].empty()
+        st.session_state['condition'] = st.session_state['col1'].empty()
+        st.session_state['last_updated'] = st.empty()
+        st.session_state['initialized'] = True
 
-    col1.metric("Location", data["location"])
-    col2.metric("Region", data["region"])
-    col1.metric("Country", data["country"])
-
-    col2.metric("Temperature (°C)", data["temp_c"])
-    col1.metric("Condition", data["condition"])
+    # Mettre à jour les placeholders avec de nouvelles données
+    st.session_state['location'].metric("Location", data["location"])
+    st.session_state['region'].metric("Region", data["region"], data["country"])
+    st.session_state['temperature'].metric("Temperature (°C)", data["temp_c"])
+    st.session_state['condition'].metric("Condition", data["condition"])
     last_updated = data["last_updated"]
-    st.success(f"Last updated: {last_updated}")
+    st.session_state['last_updated'].success(f"Last updated: {last_updated}")
 
 def consume_message():
     consumer = KafkaConsumer(
@@ -26,7 +32,6 @@ def consume_message():
     )
 
     for message in consumer:
-        print(message)
         process_data(message)
 
 if __name__ == '__main__':
--- python/src/producer.py
+++ python/src/producer.py
@@ -1,6 +1,8 @@
-import json
 import streamlit as st
 from kafka import KafkaProducer
+import json
+import threading
+from time import sleep
 from environment import KAFKA_BROKER, KAFKA_TOPIC
 from geocoding import fetch_lat_long
 from dashboards import producer_dashboard
@@ -14,8 +16,21 @@ def producer(location):
         return res
     return None
 
+def producer_loop():
+    producer = KafkaProducer(bootstrap_servers=[KAFKA_BROKER])
+    while True:
+        location = "Paris"
+        data = fetch_lat_long(location)
+        if data:    
+            res = json.dumps(data).encode("utf-8")
+            producer.send(KAFKA_TOPIC, res)
+            sleep(900)
+
 if __name__ == "__main__":
     producer_dashboard()
+    thread = threading.Thread(target=producer_loop)
+    thread.start()
+
     location = st.text_input('Location')
     action = st.button('Produce weather data to Kafka')
     if action:
