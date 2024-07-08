5ddd5070aac967c15ffb612d629ae1aac53a9927
Sebastien Zhou <Sebastien.4084@gmail.com>
2024-07-08 11:45:52 +0200

    Geocoding API

---
+++ python/src/geocoding.py
@@ -0,0 +1,24 @@
+import requests
+
+def fetch_lat_long(address: str):
+    params = {
+        "q": address,
+        "autocomplete": 1,
+        "limit": 1,
+    }
+
+    url = "https://api-adresse.data.gouv.fr/search"
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if data == {}:
+        return False
+    
+    result = {
+        "lat": data["features"][0]["geometry"]["coordinates"][1],
+        "long": data["features"][0]["geometry"]["coordinates"][0],
+    }
+    return result
+
+
+print(fetch_lat_long("15 rue de Clignancourt, Paris"))
